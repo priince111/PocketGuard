98b657fbc4396ff1ccb878a6c7d7eb9e61ba474e
prince111 <princeagrawal240@gmail.com>
2024-08-13 16:55:02 +0530

    added headers

--- server/index.js
+++ server/index.js
@@ -26,6 +26,12 @@ app.use('/api',transactionRoute);
 app.use('/api',userRoute)
 app.use('/api',otpRoute)
 
+app.use(function (request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.options('*', cors());
 
 const port = process.env.PORT || 4000;
