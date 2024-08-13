961af9ce94c2fafffe177709786d83289651c571
prince111 <princeagrawal240@gmail.com>
2024-08-13 15:14:56 +0530

    removed trailing slash

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://pocketguard-v2.vercel.app/'],
+  origin: ['http://localhost:3000', 'https://pocketguard-v2.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
